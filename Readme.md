55438da8154829f5ca176485fc13349009978136
Denis Kerner <dtricks@live.de>
2021-05-23 12:15:27 +0200

    added code for multiple consumers

--- Readme.md
+++ Readme.md
@@ -3,11 +3,17 @@
 ## Run server
 
 ```bash
-cargo run
+RUST_LOG=info cargo run .
 ```
 
 ## Run client
 
 ```bash
 cargo run --example client ws://127.0.0.1:9002
+```
+
+## JS-client
+
+```bash
+firefox examples/client-js/index.html
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use futures_util::{SinkExt, StreamExt};
 use log::*;
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
-use std::{net::SocketAddr, time::Duration};
+use std::env;
+use std::net::SocketAddr;
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::{accept_async, tungstenite::Error};
 use tungstenite::{Message, Result};
 
-async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
-    if let Err(e) = handle_connection(peer, stream).await {
+async fn accept_connection(
+    peer: SocketAddr,
+    stream: TcpStream,
+    rx: crossbeam_channel::Receiver<notify::event::Event>,
+) {
+    if let Err(e) = handle_connection(peer, stream, rx).await {
         match e {
             Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
             err => error!("Error processing connection: {}", err),
@@ -15,63 +20,47 @@ async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
     }
 }
 
-async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
+async fn handle_connection(
+    peer: SocketAddr,
+    stream: TcpStream,
+    rx: crossbeam_channel::Receiver<notify::event::Event>,
+) -> Result<()> {
     let ws_stream = accept_async(stream).await.expect("Failed to accept");
     info!("New WebSocket connection: {}", peer);
-    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
-    let mut interval = tokio::time::interval(Duration::from_millis(1000));
+    let (mut ws_sender, mut _ws_receiver) = ws_stream.split();
 
-    let (tx, rx) = crossbeam_channel::unbounded();
-    let mut watcher: RecommendedWatcher =
-        Watcher::new_immediate(move |res| tx.send(res).unwrap()).unwrap();
-    info!("created watcher");
-    watcher
-        .watch(std::path::Path::new("Readme.md"), RecursiveMode::Recursive)
-        .unwrap();
-    info!("started watching");
-    for res in rx {
-        match res {
-            Ok(event) => {
-                ws_sender
-                    .send(Message::Text(format!("{:?}\n", event)))
-                    .await?;
-                info!("{:?}", event);
-            }
-            Err(e) => error!("Watch error: {:?}", e),
-        }
+    for event in rx {
+        ws_sender
+            .send(Message::Text(format!("{:?}\n", event)))
+            .await?;
+        info!("{:?}", event);
     }
-    //loop {
-    //    tokio::select! {
-    //        msg = ws_receiver.next() => {
-    //            match msg {
-    //                Some(msg) => {
-    //                    let msg = msg?;
-    //                    if msg.is_text() ||msg.is_binary() {
-    //                        ws_sender.send(msg).await?;
-    //                    } else if msg.is_close() {
-    //                        break;
-    //                    }
-    //                }
-    //                None => break,
-    //            }
-    //        }
-    //        _ = interval.tick() => {
-    //            ws_sender.send(Message::Text("tick\n".to_owned())).await?;
-    //            info!("Tick happened");
-    //        }
-    //    }
-    //}
-
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
+    let path_str = env::args().nth(1).expect("First argument has to be a path");
 
     let addr = "127.0.0.1:9002";
     let listener = TcpListener::bind(&addr).await.expect("Can't listen");
     info!("Listening on: {}", addr);
+    let (tx, rx) = crossbeam_channel::unbounded();
+    let mut watcher: RecommendedWatcher = Watcher::new_immediate(move |res| match res {
+        Ok(event) => {
+            tx.send(event).unwrap();
+        }
+        Err(e) => {
+            error!("Watch Error {}", e);
+        }
+    })
+    .unwrap();
+    info!("created watcher");
+    watcher
+        .watch(std::path::Path::new(&path_str), RecursiveMode::Recursive)
+        .unwrap();
+    info!("started watching");
 
     while let Ok((stream, _)) = listener.accept().await {
         let peer = stream
@@ -79,6 +68,6 @@ async fn main() {
             .expect("connected streams should have a peer address");
         info!("Peer address: {}", peer);
 
-        tokio::spawn(accept_connection(peer, stream));
+        tokio::spawn(accept_connection(peer, stream, rx.clone()));
     }
 }
